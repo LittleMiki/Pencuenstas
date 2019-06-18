a3d913a289517169cacedabce0d6eaac512520e0
Miguel Angel <troodycdaw@gmail.com>
2019-06-18 22:03:46 +0200

    Mejora de Funcionalidad 1.0

--- resources/views/Gusuarios.blade.php
+++ resources/views/Gusuarios.blade.php
@@ -3,13 +3,15 @@
 <html lang="{{ app()->getLocale() }}">
     <head>
         <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
+        <script type="text/javascript" src="{{ URL::asset('js/jquery.js') }}"></script> 
+        <script src="{{asset('js/bootstrap.js')}}" type="text/javascript"></script>
+        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="container-fluid">
         @include('header')
         <div id="main" style="text-align: center">
 
